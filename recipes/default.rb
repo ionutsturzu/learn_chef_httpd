3243937dbbfb658440af2500c9a3b4e0dea8cf5e
skytap <skytap@host-1.skytap.example>
2017-09-13 03:29:13 -0700

    Added httpd configuration to cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action %i(enable start)
+end
+
+template '/var/www/html/index.html' do
+  source 'intex.html.erb'
+end
